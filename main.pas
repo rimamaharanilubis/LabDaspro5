534ad019b0964d674af0afd8f18ea6e54b09c320
rimamaharanilubis <maharanilubis250305@gmail.com>
2023-09-22 09:46:13 +0700

    kuis 1

---
+++ main.pas
@@ -0,0 +1,15 @@
+program biodata
+var
+nama,nim,kom,alamat:string;
+begin
+    write('nama =');
+    readln(nama);
+    write('nim =');
+    readln(nim);
+    write('kom =');
+    readln(kom);
+    write('alamat =');
+    readln(alamat);
+
+    writeln('nama saya ', nama,' nim saya ',nim,' kom saya ',kom,' alamat saya ',alamat,);
+end;
